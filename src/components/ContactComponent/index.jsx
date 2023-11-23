9365e5d4c5d2072b51e8b76f52268efcea2ddfbb
songlie <songlie.xu@edu.sait.ca>
2023-11-22 23:31:32 -0700

    change the landing page. etc

---
+++ src/components/ContactComponent/index.jsx
@@ -0,0 +1,33 @@
+import React, {Component} from 'react'
+import './index.css'
+
+export default class ContactComponent extends Component {
+	render() {
+		return (
+			<div className='contact'>
+				<div>
+					<h1>Have Questions? </h1>
+					<h2>Please feel free to call or email us,
+
+						or use our contact form to get in touch with us.
+
+						We look forward to hearing from you!</h2>
+
+					<h2>Emergency? Call Us:</h2>
+					+1234567890
+
+					<h2>Send Us Mail</h2>
+					info@example.com
+
+					<h2>Find the Studio</h2>
+
+
+				</div>
+				<div></div>
+
+
+			</div>
+		)
+	}
+}
+
---
+++ src/pages/ContactPage/index.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+import Contact from "../../components/Contact";
+import ContactComponent from "../../components/ContactComponent";
+
+export default class Contact extends Component {
+	render() {
+		return (
+			<div>
+				<ContactComponent></ContactComponent>
+
+
+			</div>
+		)
+	}
+}
